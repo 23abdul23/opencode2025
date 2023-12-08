b6db3f09691c13f74686090fd0b21fd3ce77eb06
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2023-12-09 02:02:54 +0530

    fixed profile page

--- src/app/admin/home/page.tsx
+++ src/app/admin/home/page.tsx
@@ -9,6 +9,8 @@ import {
   Box,
   useColorModeValue,
 } from '@chakra-ui/react';
+import { useEffect } from 'react';
+
 
 
 export default function Dashboard() {
@@ -16,7 +18,15 @@ export default function Dashboard() {
 
 const brandColor = useColorModeValue('brand.500', 'white');
   const boxBg = useColorModeValue('secondaryGray.300', 'whiteAlpha.100');
-
+  useEffect(() => {
+    const querystring = window.location.search;
+    const urlParam = new URLSearchParams(querystring);
+    const TokenParam = urlParam.get('token');
+    if(TokenParam===null){
+      window.location.assign('localhost:3000/auth/sign-in')
+    }
+    else localStorage.setItem('token',TokenParam);
+  }, []);
 
   return (
     <>
