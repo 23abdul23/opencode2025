01307762e9d9d640ba1158f07e857351adfd3ed4
Bhupesh Dewangan <bhupeshdewangan20@gmail.com>
2023-12-10 22:27:45 +0530

    title-reh-gya

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import { Box } from '@chakra-ui/react';
 import React, { ReactNode } from 'react';
 import AppWrappers from './AppWrappers';
+import RootHead from './head';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <RootHead/>
       <body id={'root'}>
         <AppWrappers>{children}</AppWrappers>
       </body>
