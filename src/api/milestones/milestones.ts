70df89858ffdf8cf5ca8c26dda1b4a9c233c8c5e
Aryan-coder06 <aryaniiitian06@gmail.com>
2026-01-17 13:42:47 +0530

    cards upgrade

--- src/api/milestones/milestones.ts
+++ src/api/milestones/milestones.ts
@@ -1,6 +1,3 @@
-const EVENT_NAME = 'OPENCODE';
-
-import { User } from 'lucide-react';
 import {UserLeaderboardData, week01Leaderboards, week02Leaderboards, week03Leaderboards,week04Leaderboards, week05Leaderboards} from '../../constants/week_wise_Leaderboard_data';
 import { EventWinners } from 'constants/event_winners_data';
 function compileWeekData(data: UserLeaderboardData[]) {
@@ -15,15 +12,6 @@ function compileWeekData(data: UserLeaderboardData[]) {
     return compiled
   }
 
-
-const avatars = [
-  'https://i.pravatar.cc/150?img=3',
-  'https://i.pravatar.cc/150?img=5',
-  'https://i.pravatar.cc/150?img=7',
-  'https://i.pravatar.cc/150?img=9',
-  'https://i.pravatar.cc/150?img=11',
-
-]
 
 export const weeklyLeaderboards = [
   {
@@ -61,66 +49,6 @@ export const weeklyLeaderboards = [
     leaderboard: compileWeekData(week05Leaderboards)
   },
 ];
-
-export const demoEventWinners = {
-  'Data Blitz': {
-    1: [
-      {
-        name: 'Aman',
-        username: 'aman-opencode',
-        avatar: avatars[1],
-        score: 620,
-        date: '2024-02-10',
-      },
-    ],
-    2: [
-      {
-        name: 'Shubham',
-        username: 'shubham-dev',
-        avatar: avatars[0],
-        score: 600,
-        date: '2024-02-10',
-      },
-    ],
-    3: [
-      {
-        name: 'Riya',
-        username: 'riya-codes',
-        avatar: avatars[2],
-        score: 580,
-        date: '2024-02-10',
-      },
-    ],
-  },
-
-  'Code Guerra': {
-    1: [
-      {
-        name: 'Shubham',
-        username: 'shubham-dev',
-        avatar: avatars[0],
-        score: 650,
-        date: '2024-02-10',
-      },
-      {
-        name: 'Aman',
-        username: 'aman-opencode',
-        avatar: avatars[1],
-        score: 650,
-        date: '2024-02-10',
-      },
-    ],
-    2: [
-      {
-        name: 'Neha',
-        username: 'neha-react',
-        avatar: avatars[4],
-        score: 610,
-        date: '2024-02-10',
-      },
-    ],
-  },
-};
 
 // =====================
 // DEMO API FUNCTIONS
@@ -165,7 +93,7 @@ export const fetchWeekLeaderboard = async (week: number) => {
 
 // Fetch specific event winners by repo name
 export const fetchEventWinnersByName = async (repoName: string) => {
-  const repoData = demoEventWinners[repoName as keyof typeof demoEventWinners];
+  const repoData = EventWinners[repoName as keyof typeof EventWinners];
 
   if (!repoData) {
     return Promise.resolve({
