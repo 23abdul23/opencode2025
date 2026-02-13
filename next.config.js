b70d1adc108d0f65724831f5f56a01b6e6613397
23abdul23 <23abdulazeem23@gmail.com>
2026-02-13 19:49:21 +0530

    deployed

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'export', // CRITICAL: Tells Next.js to generate static HTML/CSS/JS
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '', 
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  output: 'export',
+  basePath: '/opencode2025',
+  assetPrefix: '/opencode2025',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'i.ibb.co' },
       { protocol: 'https', hostname: 'scontent.fotp8-1.fna.fbcdn.net' },
     ],
-    unoptimized: true,
   },
 };
 
