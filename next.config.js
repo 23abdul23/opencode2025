a0e0d10ba616950d0bd79a1325225301dcb130dc
23abdul23 <23abdulazeem23@gmail.com>
2026-02-13 19:33:19 +0530

    deploy

--- next.config.js
+++ next.config.js
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  output: 'export', // CRITICAL: Tells Next.js to generate static HTML/CSS/JS
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '', 
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'i.ibb.co' },
       { protocol: 'https', hostname: 'scontent.fotp8-1.fna.fbcdn.net' },
     ],
-    // Make ENV
     unoptimized: true,
   },
 };
 
-// module.exports = withTM(nextConfig);
 module.exports = nextConfig;
